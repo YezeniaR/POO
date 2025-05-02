6b52d0ad27f9d785e9128bc9f6932dbda5039aaa
Yezenia Restrepo <125924954+YezeniaR@users.noreply.github.com>
2025-05-01 23:15:22 -0500

    Update Ejercicio 2.1.py

--- Actividad 2/Ejercicio 2.1.py
+++ Actividad 2/Ejercicio 2.1.py
@@ -1,1 +1,22 @@
+class Persona:
+    def __init__(self, nombre, apellidos, documento_identidad, año_nacimiento):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.documento_identidad = documento_identidad
+        self.año_nacimiento = año_nacimiento
+    
+    def imprimir(self):
+        print("Nombre =", self.nombre)
+        print("Apellidos =", self.apellidos)
+        print("Número de documento de identidad =", self.documento_identidad)
+        print("Año de nacimiento =", self.año_nacimiento)
+        print()
 
+def main():
+    p1 = Persona("Pedro", "Pérez", "1053121010", 1998)
+    p2 = Persona("Luis", "León", "1053223344", 2001)
+    p1.imprimir()
+    p2.imprimir()
+
+if __name__ == "__main__":
+    main()
