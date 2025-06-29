68734cab101ecba4d354c34356f40e4d04c99c50
Yezenia Restrepo <125924954+YezeniaR@users.noreply.github.com>
2025-06-29 18:12:41 -0500

    Update Ejercicio 1.py

--- Actividad 5/Ejercicio 1.py
+++ Actividad 5/Ejercicio 1.py
@@ -1,1 +1,31 @@
+class PruebaExcepciones:
+    @staticmethod
+    def main():
+        # Primer bloque try
+        try:
+            print("Ingresando al primer try")
+            cociente = 10000 / 0  # Se lanza una excepción
+            print("Después de la división") 
+        except ZeroDivisionError as e:  # Se captura la excepción
+            print("División por cero")  
+        finally:
+            print("Ingresando al primer finally")
+
+        # Segundo bloque try
+        try:
+            print("Ingresando al segundo try")
+            objeto = None
+            str(objeto)  # Se lanza una excepción
+            print("Imprimiendo objeto")
+        except ZeroDivisionError as e:
+            print("División por cero")
+        except Exception as e:  # Se captura la excepción
+            print("Ocurrió una excepción")  # Se imprime en pantalla este mensaje
+        finally:
+            print("Ingresando al segundo finally")
+
+
+# Ejecutar el método main
+if __name__ == "__main__":
+    PruebaExcepciones.main()
 
